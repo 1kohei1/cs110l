b1a6df0f1347f7ca3e0fc65c5edc27f23369aac2
Kohei Arai <tobecomebig@gmail.com>
2021-06-25 22:47:40 -0700

    Completed first assignment

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -7,15 +7,34 @@ fn main() {
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    let mut newv: Vec<i32> = Vec::new();
+
+    for num in v.iter() {
+        newv.push(num + n);
+    }
+    newv
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    let mut index:usize = 0;
+    while index < v.len() {
+        v[index] = v[index] + n;
+        index += 1;
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    let mut index:usize = 1;
+    let mut seen:HashSet<i32> = HashSet::new();
+
+    while index < v.len() {
+        if seen.contains(&v[index]) {
+            v.remove(index);
+        } else {
+            seen.insert(v[index]);
+            index += 1;
+        }
+    }
 }
 
 #[cfg(test)]
