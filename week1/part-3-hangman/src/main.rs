cb1b5235e6928738c54dd168ba883202d1c4a6d2
Kohei Arai <tobecomebig@gmail.com>
2021-06-25 23:54:45 -0700

    Created a hangman

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -27,14 +27,104 @@ fn pick_a_random_word() -> String {
     String::from(words[rand::thread_rng().gen_range(0, words.len())].trim())
 }
 
+fn print_word_so_far(revealed_indexes: &Vec<bool>, secret_word_chars: &Vec<char>) {
+    let mut s: String = String::new();
+    for i in 0..secret_word_chars.len() {
+        if revealed_indexes[i] {
+            s.push(secret_word_chars[i]);
+        } else {
+            s.push('-');
+        }
+    }
+
+    println!("The word so far is {}", s);
+}
+
+fn print_guessed_so_far(guessed_so_far: &Vec<char>) {
+    let mut s: String = String::new();
+    for i in 0..guessed_so_far.len() {
+        s.push(guessed_so_far[i]);
+    }
+    println!("You have guessed the following letters: {}", s);
+}
+
+fn print_num_guess_left(num_guess_left: u32) {
+    println!("You have {} guesses left", num_guess_left);
+}
+
+fn get_guess() -> char {
+    print!("Please guess a letter: ");
+    io::stdout().flush().expect("Error flushing stdout.");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading line.");
+    guess.chars().next().unwrap()
+}
+
+fn process_user_input(
+    revealed_indexes: &mut Vec<bool>,
+    secret_word_chars: &Vec<char>,
+    c: char,
+    num_guess_left: u32,
+) -> u32 {
+    let mut found: bool = false;
+    let mut returnv = num_guess_left;
+
+    for i in 0..secret_word_chars.len() {
+        if secret_word_chars[i] == c {
+            revealed_indexes[i] = true;
+            found = true;
+        }
+    }
+
+    if !found {
+        returnv -= 1;
+        println!("Sorry, that letter is not in the word");
+    }
+    returnv
+}
+
+fn print_result(num_guess_left: u32, secret_word: &String) {
+    if num_guess_left == 0 {
+        println!("Sorry, you ran out of guesses!");
+    } else {
+        println!(
+            "Congratulations you guessed the secret word: {}",
+            secret_word
+        );
+    }
+}
+
 fn main() {
     let secret_word = pick_a_random_word();
     // Note: given what you know about Rust so far, it's easier to pull characters out of a
     // vector than it is to pull them out of a string. You can get the ith character of
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
-    // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
+    let mut revealed_indexes: Vec<bool> = Vec::new();
+    let mut guessed_so_far: Vec<char> = Vec::new();
+    let mut num_guess_left = NUM_INCORRECT_GUESSES;
 
-    // Your code here! :)
+    for _i in 0..secret_word.len() {
+        revealed_indexes.push(false);
+    }
+
+    println!("Welcome to CS110L Hangman!");
+
+    while 0 < num_guess_left {
+        print_word_so_far(&revealed_indexes, &secret_word_chars);
+        print_guessed_so_far(&guessed_so_far);
+        print_num_guess_left(num_guess_left);
+
+        let c: char = get_guess();
+        num_guess_left =
+            process_user_input(&mut revealed_indexes, &secret_word_chars, c, num_guess_left);
+        guessed_so_far.push(c);
+
+        println!();
+    }
+
+    print_result(num_guess_left, &secret_word);
 }
