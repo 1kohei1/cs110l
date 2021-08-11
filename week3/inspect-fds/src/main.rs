a885853d1213777f273a5530da4ba047530d05be
Kohei Arai <tobecomebig@gmail.com>
2021-08-11 11:54:34 +0000

    Completed milestone 1.

--- week3/inspect-fds/src/main.rs
+++ week3/inspect-fds/src/main.rs
@@ -10,11 +10,18 @@ fn main() {
         println!("Usage: {} <name or pid of target>", args[0]);
         std::process::exit(1);
     }
-    #[allow(unused)] // TODO: delete this line for Milestone 1
+    
     let target = &args[1];
+    let target_process = ps_utils::get_target(target).expect("Failed to get the target");
 
-    // TODO: Milestone 1: Get the target Process using psutils::get_target()
-    unimplemented!();
+    match target_process {
+        Some(process) => println!("Found pid {}", process.pid),
+        None => {
+            let error_message = format!("Target \"{}\" did not match any running PIDs or executables", target);
+            println!("{}", error_message);
+            std::process::exit(1);
+        }
+    }
 }
 
 #[cfg(test)]
